ed9b787023e5a196797527d478b47d4a77eb6b7b
Jack Casey <jack.casey@agworld.com.au>
2014-04-09 19:12:40 +0800

    Split implementation into lib and cli.

---
+++ pazzlib.rb
@@ -0,0 +1,24 @@
+#! /usr/bin/ruby
+require 'digest/sha2'
+require 'digest/sha1'
+
+class Pazz
+
+  ALPHA_NUMERIC       = ('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a
+  AMBIGUOUS_TOKENS    = %w( 1 l I O o 0 )
+  SPECIAL_CHARACTERS  = %w( ! @ # $ % ^ & * - _ ? )
+  TOKENS =  ALPHA_NUMERIC + SPECIAL_CHARACTERS - AMBIGUOUS_TOKENS
+
+  def initialize(password)
+    # SHA it and store SHA2 for the seed
+    @seed = Digest::SHA2.hexdigest(password)
+  end
+
+  def get_pass(site)
+    Digest::SHA1.hexdigest(@seed + site)  # use SHA on seed + site
+      .scan(/../)                         # pull out hex pairs
+      .map(&:hex)                         # parse hex to dec
+      .map{|x| TOKENS[x % TOKENS.length]}  # use to index token array
+      .join                               # return as string of tokens
+  end
+end
